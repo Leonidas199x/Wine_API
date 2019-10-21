ffe3376ea50b29214a5a3d2987bc484341dc3340
Leonidas199x <toby.w.prince@outlook.com>
2019-10-21 19:26:28 +0100

    Addition of lookups controller

---
+++ Wine_API/Wine_API/Controllers/LookupsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WineService.Countries;
+using System.Linq;
+using System.Threading.Tasks;
+using DataContract.Country;
+
+namespace Wine_API.Controllers
+{
+    [Route("[controller]")]
+    public class LookupsController : Controller
+    {
+        private ICountryService _countryService;
+
+        public LookupsController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet("countries")]
+        public async Task<IActionResult> GetAllCountries()
+        {
+            var countries = await _countryService.GetAll().ConfigureAwait(false);
+
+            if (!countries.Any())
+            {
+                return NotFound(); 
+            }
+            
+            return Ok(countries);
+        }
+    }
+}
