fb06fba50856414c812ef7a9e4270fa30c033480
Leonidas199x <toby.w.prince@outlook.com>
2021-03-21 18:47:21 +0000

    Start of unit testing for API

---
+++ src/Controller Unit Tests/CountryControllerUnitTests/WhenTestingTheClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using WineApi.Controllers;
+
+namespace CountryControllerUnitTests
+{
+    public abstract class WhenTestingTheClass
+    {
+        public ICountryService CountryService { get; private set; }
+
+        public IMapper CountryMapper { get; private set; }
+
+        protected CountryController ItemUnderTest { get; private set; }
+
+        protected abstract void ArrangeScenario();
+
+        protected abstract Task Run();
+    }
+}
