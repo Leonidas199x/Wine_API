a733ae0198357504bda078ee8768352fc6eaff0d
Leonidas199x <toby.w.prince@gmail.com>
2021-04-06 22:36:34 +0100

    Addition of check to see if country already exists

--- src/Domain/Countries/CountryValidator.cs
+++ src/Domain/Countries/CountryValidator.cs
@@ -1,16 +1,36 @@
 ﻿using FluentValidation;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Domain.Countries
 {
     public class CountryValidator : AbstractValidator<Country>
     {
-        public CountryValidator()
+        private readonly ICountryRepository _countryRepository;
+
+        public CountryValidator(ICountryRepository countryRepository)
         {
+            _countryRepository = countryRepository;
+
             RuleFor(x => x.Name)
                 .NotEmpty()
                 .WithMessage("Name is required")
                 .MaximumLength(50)
                 .WithMessage("Name cannot be more that 50 characters");
+
+
+            RuleFor(x => x)
+                .MustAsync(async (country, context, cancellation) =>
+                {
+                    return await CountryExists(country.Name).ConfigureAwait(false);
+                })
+                .WithMessage($"Country already exists");
+        }
+
+        private async Task<bool> CountryExists(string name)
+        {
+            var country = await _countryRepository.GetByName(name).ConfigureAwait(false);
+            return !country.Any(x => x.Name == name);
         }
     }
 }
