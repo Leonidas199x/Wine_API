42952570d590b3836c9d4cf706307251f7bf3695
Leonidas199x <toby.w.prince@gmail.com>
2021-04-09 17:52:48 +0100

    Addition of get grape colour by colour sproc

---
+++ src/Database/Wine/sprocs/dbo.GrapeColour_GetByColour.sql
@@ -0,0 +1,16 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[GrapeColour_GetByColour]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[GrapeColour_GetByColour] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].GrapeColour_GetByColour
+    @GrapeColour VARCHAR(20)
+AS
+BEGIN
+
+    SELECT
+        GC.[ID],
+        GC.[Colour]
+    FROM [dbo].[GrapeColour] AS	GC
+    WHERE GC.[Colour] = @GrapeColour;
+
+END
