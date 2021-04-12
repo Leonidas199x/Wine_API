8c88d5c5470b439ceacadaaaa8f14ef946570c5a
Leonidas199x <toby.w.prince@gmail.com>
2021-04-12 20:55:04 +0100

    Removal of colour, and addition of IsNew property

--- src/Domain/Grapes/Grape.cs
+++ src/Domain/Grapes/Grape.cs
@@ -10,12 +10,12 @@ namespace Domain
 
         public int GrapeColourId { get; set; }
 
-        public string Colour { get; set; }
-
         public string Note { get; set; }
 
         public DateTime DateCreated { get; set; }
 
         public DateTime DateUpdated { get; set; }
+
+        public bool IsNew => Id == default;
     }
 }
