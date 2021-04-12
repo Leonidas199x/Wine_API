2434be18e77ce5fa7eec013c5179a701b2af3f18
Leonidas199x <toby.w.prince@gmail.com>
2021-04-12 20:31:40 +0100

    Addition of Grape sprocs

---
+++ src/Database/Wine/sprocs/dbo.Grape_GetByName.sql
@@ -0,0 +1,22 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Grape_GetByName]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Grape_GetByName] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Grape_GetByName]
+    @GrapeName VARCHAR(25)
+AS
+BEGIN
+
+    SELECT
+        G.[ID],
+        G.[Name],
+        G.[GrapeColourID],
+        GC.[Colour],
+        G.[Note],
+        G.[DateCreated],
+        G.[DateUpdated]
+    FROM [dbo].[Grape] AS G
+    LEFT JOIN [dbo].[GrapeColour] AS GC ON G.[GrapeColourID] = GC.[ID]
+    WHERE G.[Name] = @GrapeName;
+
+END
