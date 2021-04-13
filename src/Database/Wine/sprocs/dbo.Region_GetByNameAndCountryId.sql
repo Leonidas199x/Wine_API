b40f7443901d6f812002c542965a26655dbd3949
Leonidas199x <toby.w.prince@gmail.com>
2021-04-13 19:41:00 +0100

    Addition of GetById & GetByNameAndCountryId sprocs

---
+++ src/Database/Wine/sprocs/dbo.Region_GetByNameAndCountryId.sql
@@ -0,0 +1,23 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Region_GetByNameAndCountryId]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Region_GetByNameAndCountryId] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Region_GetByNameAndCountryId]
+    @RegionName NVARCHAR(50),
+    @CountryId INT
+AS
+BEGIN
+
+    SELECT
+        R.[ID],
+        R.[CountryID],
+        R.[Name],
+        R.[Note],
+        R.[LocationRegionImage],
+        R.[DateCreated],
+        R.[DateUpdated]
+    FROM [dbo].[Region] AS R
+    WHERE R.[Name] = @RegionName
+        AND R.[CountryId] = @CountryId;
+
+END
