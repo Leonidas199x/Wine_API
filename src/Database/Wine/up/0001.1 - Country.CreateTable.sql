37f9d1206f65ba96f4d9714aa9357f603e61c3bc
Leonidas199x <toby.w.prince@gmail.com>
2021-04-15 14:02:46 +0100

    Amendment to make ISO_Code not null

--- src/Database/Wine/up/0001.1 - Country.CreateTable.sql
+++ src/Database/Wine/up/0001.1 - Country.CreateTable.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Country](
     [ID] INT IDENTITY(1,1) NOT NULL
         PRIMARY KEY,
-    [Name] NVARCHAR(50) NULL,
-    [ISO_Code] NVARCHAR(2),
+    [Name] NVARCHAR(50) NOT NULL,
+    [ISO_Code] NVARCHAR(2) NOT NULL,
     [Note] NVARCHAR(MAX) NULL,
     [DateCreated] DATETIME2(3) NOT NULL,
     [DateUpdated] DATETIME2(3) NOT NULL
