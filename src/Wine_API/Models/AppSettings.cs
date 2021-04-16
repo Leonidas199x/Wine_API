8b6c12639cefbd461864aa3f18b78b62a8488010
Leonidas199x <toby.w.prince@gmail.com>
2021-04-16 12:55:56 +0100

    Amendment to appsettings

--- src/Wine_API/Models/AppSettings.cs
+++ src/Wine_API/Models/AppSettings.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WineAPI.Models
+﻿namespace WineAPI.Models
 {
     public class AppSettings
     {
-        public string Environment { get; set; }
+        public string Key { get; set; }
 
-        public string ConnectionString { get; set; }
+        public string Value { get; set; }
     }
 }
