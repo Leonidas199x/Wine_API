fcd247c8f38f3a3c7c82116c9c7ef8aa6148abfa
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 12:17:45 +0100

    Made name of producer not null

--- src/Database/Wine/up/0005.1 - Producer.CreateTable.sql
+++ src/Database/Wine/up/0005.1 - Producer.CreateTable.sql
@@ -2,7 +2,7 @@
 (
     [ID] INT IDENTITY(1,1) NOT NULL
         PRIMARY KEY,
-    [Name] NVARCHAR(50) NULL,
+    [Name] NVARCHAR(50) NOT NULL,
     [Note] NVARCHAR(MAX) NULL,
     [DateCreated] DATETIME2(3) NULL,
     [DateUpdated] DATETIME2(3) NULL
