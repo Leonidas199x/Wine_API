a8050d06ff38fceac20a0bc4d0c5cce643e58488
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 12:18:14 +0100

    Addition of Producer domain files

---
+++ src/Domain/Producer/IProducerRepository.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Producer
+{
+    public interface IProducerRepository
+    {
+        Task<IEnumerable<Producer>> GetAll();
+
+        Task<Producer> Get(int Id);
+
+        Task<ValidationResult> Insert(Producer producer);
+
+        Task<ValidationResult> Update(Producer producer);
+
+        Task Delete(int id);
+
+
+    }
+}
---
+++ src/Domain/Producer/ProducerValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Domain.Producer
+{
+    public class ProducerValidator : AbstractValidator<Producer>
+    {
+        public ProducerValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .MaximumLength(50)
+                .WithMessage("Name cannot be more than 50 characters");
+        }
+    }
+}
