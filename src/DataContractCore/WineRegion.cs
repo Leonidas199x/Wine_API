425862a2fa3d2675df3084f287a9136d8b94c73e
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 11:20:39 +0100

    Addition of data contract wine region objects

---
+++ src/DataContractCore/WineRegion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataContract
+{
+    public class WineRegion
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RegionId { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    }
+}
---
+++ src/DataContractCore/WineRegionCreate.cs
@@ -0,0 +1,11 @@
+﻿namespace DataContract
+{
+    public class WineRegionCreate
+    {
+        public string Name { get; set; }
+
+        public int RegionId { get; set; }
+
+        public string Note { get; set; }
+    }
+}
