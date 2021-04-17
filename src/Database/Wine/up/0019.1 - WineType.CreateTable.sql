5b14d422291e7bd454bde81906c64af4ff6a76c0
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 11:30:43 +0100

    Removal of dupe create table script, and renumbering

--- src/Database/Wine/up/0019.1 - WineType.CreateTable.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[WineType]
-(
-    [ID] INT IDENTITY(1,1) NOT NULL
-        PRIMARY KEY,
-    [Name] NVARCHAR(50) NOT NULL,
-    [Note] NVARCHAR(MAX) NULL,
-    [DateCreated] DATETIME2(3) NOT NULL,
-    [DateUpdated] DATETIME2(3) NOT NULL
-);
