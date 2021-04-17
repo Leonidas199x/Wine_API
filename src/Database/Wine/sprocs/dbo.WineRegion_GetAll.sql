acc6051708414c77717b30e8b8f61e696d074e37
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 10:31:50 +0100

    Addition of wine region sprocs

---
+++ src/Database/Wine/sprocs/dbo.WineRegion_GetAll.sql
@@ -0,0 +1,20 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[WineRegion_GetAll]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[WineRegion_GetAll] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[WineRegion_GetAll]
+
+AS
+BEGIN
+
+    SELECT
+        WR.[ID],
+        WR.[Name],
+        WR.[RegionId],
+        WR.[Note],
+        WR.[DateCreated],
+        WR.[DateUpdated]
+    FROM [dbo].[WineRegion] AS WR
+    ORDER BY R.[Name] ASC;
+
+END
---
+++ src/Database/Wine/sprocs/dbo.WineRegion_Update.sql
@@ -0,0 +1,23 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[WineRegion_Update]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[WineRegion_Update] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[WineRegion_Update]
+    @ID INT,
+    @Name NVARCHAR(50),
+    @RegionId INT,
+    @Note NVARCHAR(MAX)
+AS
+BEGIN
+
+    DECLARE @RunDate DATETIME = GETDATE();
+
+    UPDATE WR
+    SET
+        WR.[Name] = @Name,
+        WR.[RegionId] = @RegionId,
+        WR.[Note] = @Note
+    FROM [dbo].[WineRegion] WR
+    WHERE WR.[ID] = @ID;
+
+END
