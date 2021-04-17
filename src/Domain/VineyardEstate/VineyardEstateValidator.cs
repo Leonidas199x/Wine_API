09a05ede514696f3ebb326144dc8f5df55324f70
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 13:03:15 +0100

    Addition of domain files for vineyard estate

---
+++ src/Domain/VineyardEstate/VineyardEstateValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Domain.VineyardEstate
+{
+    public class VineyardEstateValidator : AbstractValidator<VineyardEstate>
+    {
+        public VineyardEstateValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .MaximumLength(50)
+                .WithMessage("Name cannot be more that 50 characters");
+        }
+    }
+}
