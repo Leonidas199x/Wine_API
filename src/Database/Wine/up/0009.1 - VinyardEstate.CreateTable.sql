36a8231c06dba2e3edd5e5b01ff2f1dcdd201a08
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 12:45:09 +0100

    Renamed VineyardEstateNote to Note

--- src/Database/Wine/up/0009.1 - VinyardEstate.CreateTable.sql
+++ src/Database/Wine/up/0009.1 - VinyardEstate.CreateTable.sql
@@ -3,7 +3,7 @@
     [ID] INT IDENTITY(1,1) NOT NULL
         PRIMARY KEY,
     [Name] NVARCHAR(50) NOT NULL,
-    [VinyardEstateNote] NVARCHAR(MAX) NULL,
+    [Note] NVARCHAR(MAX) NULL,
     [Longitude] DECIMAL(9,6) NULL,
     [Latitude] DECIMAL(8,6) NULL,
     [DateCreated] DATETIME2(3) NOT NULL,
