de9f70e530032a7a1a24633736819daba70e0ea0
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 12:04:36 +0100

    Addition of Producer delete sproc

---
+++ src/Database/Wine/sprocs/dbo.Producer_Delete.sql
@@ -0,0 +1,16 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Producer_Delete]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Producer_Delete] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Producer_Delete]
+    @ProducerId INT
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    DELETE P
+    FROM [dbo].[Producer] AS P
+    WHERE P.[ID] = @ProducerId;
+
+END
