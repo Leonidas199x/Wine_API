53f9ed8e968cc4e7cafea1c388d9e5dc2c594c07
Leonidas199x <toby.w.prince@gmail.com>
2021-04-17 12:45:30 +0100

    Addition of Vineyard Estate sprocs

---
+++ src/Database/Wine/sprocs/dbo.VineyardEstate_Delete.sql
@@ -0,0 +1,14 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[VineyardEstate_Delete]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[VineyardEstate_Delete] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[VineyardEstate_Delete]
+    @VineyardEstateId INT
+AS
+BEGIN
+
+    DELETE VE
+    FROM [dbo].[VineyardEstate] AS VE
+    WHERE VE.[Id] = @VineyardEstateId;
+
+END
---
+++ src/Database/Wine/sprocs/dbo.VineyardEstate_GetById.sql
@@ -0,0 +1,21 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[VineyardEstate_GetById]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[VineyardEstate_GetById] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[VineyardEstate_GetById]
+    @VineyardEstateId INT
+AS
+BEGIN
+
+    SELECT
+        VE.[ID],
+        VE.[Name],
+        VE.[Note],
+        VE.[Longitude],
+        VE.[Latitude],
+        VE.[DateCreated],
+        VE.[DateUpdated]
+    FROM [dbo].[VineyardEstate] AS VE
+    WHERE VE.[Id] = @VineyardEstateId;
+
+END
