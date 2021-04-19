74eb6aaa2ef464bf9a16eca4ce65c63145fb59a7
Leonidas199x <toby.w.prince@gmail.com>
2021-04-19 19:11:46 +0100

    Addition of unit test for CoutryService Insert

---
+++ src/Domain.UnitTests/Countries/CountryUnitTests/WhenTestingTheClass.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Countries;
+using FluentValidation;
+using NSubstitute;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Domain.UnitTests.Countries.CountryUnitTests
+{
+    [TestFixture]
+    public abstract class WhenTestingTheClass
+    {
+        protected ICountryRepository CountryRepository { get; private set; }
+
+        protected IValidator<Country> CountryValidator { get; private set; }
+
+        protected CountryService ItemUnderTest { get; private set; }
+
+        protected abstract void SetupScenario();
+
+        protected abstract Task Run();
+
+        [OneTimeSetUp]
+        public Task ArrangeScenario()
+        {
+            CountryRepository = Substitute.For<ICountryRepository>();
+
+            CountryValidator = Substitute.For<IValidator<Country>>();
+
+            SetupScenario();
+
+            ItemUnderTest = new CountryService(CountryRepository, CountryValidator);
+
+            return Run();
+        }
+    }
+}
