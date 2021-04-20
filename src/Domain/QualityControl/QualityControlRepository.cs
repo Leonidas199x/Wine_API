7d672db7d35e1376edf1bb91c723c19a8c7e7707
Leonidas199x <toby.w.prince@gmail.com>
2021-04-20 16:15:56 +0100

    Addition of QualityControl domain files

---
+++ src/Domain/QualityControl/QualityControlRepository.cs
@@ -0,0 +1,112 @@
+﻿using Dapper;
+using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Domain.QualityControl
+{
+    public class QualityControlRepository : IQualityControlRepository
+    {
+        private readonly string _connectionString;
+
+        public QualityControlRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<QualityControl>> GetAll()
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            return await connection.QueryAsync<QualityControl>(
+                "[dbo].[QualityControl_GetAll]",
+                commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+        }
+
+        public async Task<QualityControl> Get(int Id)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@QualityControlId", Id, DbType.Int32, ParameterDirection.Input);
+
+            using var connection = new SqlConnection(_connectionString);
+
+            return await connection.QuerySingleOrDefaultAsync<QualityControl>(
+                "[dbo].[QualityControl_GetById]",
+                parameters,
+                commandType: CommandType.StoredProcedure)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<QualityControl>> GetByNameAndCountry(string name, int countryId)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Name", name, DbType.String, ParameterDirection.Input);
+            parameters.Add("@CountryId", countryId, DbType.Int32, ParameterDirection.Input);
+
+            using var connection = new SqlConnection(_connectionString);
+
+            return await connection.QueryAsync<QualityControl>(
+                "[dbo].[QualityControl_GetByNameAndCountryId]",
+                parameters,
+                commandType: CommandType.StoredProcedure)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<ValidationResult> Insert(QualityControl qualityControl)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Name", qualityControl.Name, DbType.String, ParameterDirection.Input);
+            parameters.Add("@Note", qualityControl.Note, DbType.String, ParameterDirection.Input);
+            parameters.Add("@CountryID", qualityControl.CountryId, DbType.Int32, ParameterDirection.Input);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.QueryAsync(
+                    "[dbo].[QualityControl_Insert]",
+                    parameters,
+                    commandType: CommandType.StoredProcedure)
+                    .ConfigureAwait(false);
+            }
+
+            return new ValidationResult();
+        }
+
+        public async Task<ValidationResult> Update(QualityControl qualityControl)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@QualityControlId", qualityControl.Id, DbType.String, ParameterDirection.Input);
+            parameters.Add("@Name", qualityControl.Name, DbType.String, ParameterDirection.Input);
+            parameters.Add("@Note", qualityControl.Note, DbType.String, ParameterDirection.Input);
+            parameters.Add("@CountryId", qualityControl.CountryId, DbType.Int32, ParameterDirection.Input);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.QueryAsync(
+                    "[dbo].[QualityControl_Update]",
+                    parameters,
+                    commandType: CommandType.StoredProcedure)
+                    .ConfigureAwait(false);
+            }
+
+            return new ValidationResult();
+        }
+
+        public async Task<ValidationResult> Delete(int qualityControlId)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@QualityControlId", qualityControlId, DbType.Int32, ParameterDirection.Input);
+
+            using var connection = new SqlConnection(_connectionString);
+
+            await connection.QueryAsync<QualityControl>(
+                "[dbo].[QualityControl_Delete]",
+                parameters,
+                commandType: CommandType.StoredProcedure)
+                .ConfigureAwait(false);
+
+            return new ValidationResult();
+        }
+    }
+}
---
+++ src/Domain/QualityControl/QualityControlValidator.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Countries;
+using FluentValidation;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.QualityControl
+{
+    public class QualityControlValidator : AbstractValidator<QualityControl>
+    {
+        private readonly IQualityControlRepository _qualityControlRepository;
+        private readonly ICountryRepository _countryRepository;
+
+        public QualityControlValidator(
+            IQualityControlRepository qualityControlRepository, 
+            ICountryRepository countryRepository)
+        {
+            _qualityControlRepository = qualityControlRepository;
+            _countryRepository = countryRepository;
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .MaximumLength(50)
+                .WithMessage("Name cannot be more that 50 characters");
+
+            RuleFor(x => x.CountryId)
+                .MustAsync(async (qualityControl, context, cancellation) =>
+                {
+                    return await CountryExists(qualityControl.CountryId)
+                    .ConfigureAwait(false);
+                })
+                .WithMessage("Country does not exists");
+
+            RuleFor(x => x)
+                .MustAsync(async (qualityControl, context, cancellation) =>
+                {
+                    return await Exists(qualityControl.Name, qualityControl.CountryId)
+                    .ConfigureAwait(false);
+                })
+                .WithMessage("Quality Control with that name and country already exists"); ;
+        }
+
+        private async Task<bool> Exists(string name, int countryId)
+        {
+            var qualityControl = await _qualityControlRepository.GetByNameAndCountry(name, countryId).ConfigureAwait(false);
+            return !qualityControl.Any(x => x.Name == name && x.CountryId == countryId);
+        }
+
+        private async Task<bool> CountryExists(int countryId)
+        {
+            var country = await _countryRepository.Get(countryId).ConfigureAwait(false);
+            return country != null;
+        }
+    }
+}
