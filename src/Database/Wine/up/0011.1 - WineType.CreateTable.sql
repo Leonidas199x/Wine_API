18fd8d57151b9d8473e8ca920fbe022051779494
Leonidas199x <toby.w.prince@gmail.com>
2021-04-20 17:17:39 +0100

    Rename of column WineType to Name

--- src/Database/Wine/up/0011.1 - WineType.CreateTable.sql
+++ src/Database/Wine/up/0011.1 - WineType.CreateTable.sql
@@ -2,7 +2,7 @@
 (
     [ID] INT IDENTITY(1,1) NOT NULL
         PRIMARY KEY,
-    [WineType] NVARCHAR(50) NULL,
+    [Name] NVARCHAR(50) NULL,
     [Note] NVARCHAR(MAX) NULL,
     [DateCreated] DATETIME2(3) NOT NULL,
     [DateUpdated] DATETIME2(3) NOT NULL
