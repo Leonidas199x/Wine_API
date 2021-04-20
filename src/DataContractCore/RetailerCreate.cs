f1423f41c6cbf798b51b7f750a3688880e8f04f4
Leonidas199x <toby.w.prince@gmail.com>
2021-04-20 21:52:01 +0100

    Amendment to make props on retailer nullable

--- src/DataContractCore/RetailerCreate.cs
+++ src/DataContractCore/RetailerCreate.cs
@@ -4,23 +4,23 @@
     {
         public string Name { get; set; }
 
-        public int MinimumPurchaseQuantity { get; set; }
+        public int? MinimumPurchaseQuantity { get; set; }
 
-        public int IncrementQuantity { get; set; }
+        public int? IncrementQuantity { get; set; }
 
-        public decimal GenericDiscountPercentage { get; set; }
+        public decimal? GenericDiscountPercentage { get; set; }
 
         public string GenericDiscountName { get; set; }
 
         public string WebsiteUrl { get; set; }
 
-        public int WebsiteRating { get; set; }
+        public int? WebsiteRating { get; set; }
 
-        public int OrderRating { get; set; }
+        public int? OrderRating { get; set; }
 
-        public int DeliveryRating { get; set; }
+        public int? DeliveryRating { get; set; }
 
-        public int MaxCustomerRating { get; set; }
+        public int? MaxCustomerRating { get; set; }
 
         public string Note { get; set; }
     }
