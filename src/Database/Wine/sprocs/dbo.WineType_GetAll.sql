c27078ffa4ac32a39614a0756a39fa0703619f8f
Leonidas199x <toby.w.prince@gmail.com>
2021-04-20 17:17:48 +0100

    Addition of WineType sprocs

---
+++ src/Database/Wine/sprocs/dbo.WineType_GetAll.sql
@@ -0,0 +1,19 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[WineType_GetAll]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[WineType_GetAll] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[WineType_GetAll]
+
+AS
+BEGIN
+
+    SELECT
+        WT.[ID],
+        WT.[Name],
+        WT.[Note],
+        WT.[DateCreated],
+        WT.[DateUpdated]
+    FROM [dbo].[WineType] AS WT
+    ORDER BY WT.[Name] ASC;
+
+END
---
+++ src/Database/Wine/sprocs/dbo.WineType_Update.sql
@@ -0,0 +1,20 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[WineType_Update]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[WineType_Update] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[WineType_Update]
+    @WineTypeId INT,
+    @Name NVARCHAR(50),
+    @Note NVARCHAR(MAX)
+AS
+BEGIN
+
+    UPDATE WT
+    SET
+        WT.[Name] = @Name,
+        WT.[Note] = @Note,
+        WT.[DateUpdated] = GETDATE()
+    FROM [dbo].[WineType] AS WT
+    WHERE WT.[ID] = @WineTypeId;
+
+END
