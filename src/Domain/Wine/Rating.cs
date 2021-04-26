185eee2a21986443c2423013760327e96e408447
Leonidas199x <toby.w.prince@gmail.com>
2021-04-26 13:47:50 +0100

    Addition of Wine and WineRating objects

---
+++ src/Domain/Wine/Rating.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Wine
+{
+    public class WineRating
+    {
+        public int Id { get; set; }
+
+        public int DrinkerId { get; set; }
+
+        public int WineId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Note { get; set; }
+    }
+}
---
+++ src/Domain/Wine/Wine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Wine
+{
+    public class Wine
+    {
+        public int Id { get; set; }
+
+        public int ProducerId { get; set; }
+
+        public int RegionId { get; set; }
+
+        public int Vintage { get; set; }
+
+        public int QualityControlId { get; set; }
+
+        public int VineyardEstateId { get; set; }
+
+        public int WineTypeId { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Abv { get; set; }
+
+        public string Importer { get; set; }
+
+        public int InventoryLevel { get; set; }
+
+        public int ExclusiveRetailerId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+        public IEnumerable<WineRating> Ratings { get; set; }
+
+        public IEnumerable<Grape> Grapes { get; set; }
+    }
+}
