81b70bbd6adf1c26786c6a67308c5c9151b85909
Leonidas199x <toby.w.prince@gmail.com>
2023-09-13 20:38:28 +0100

    Added paging info

--- src/Database/Wine/sprocs/dbo.VineyardEstate_GetAll.sql
+++ src/Database/Wine/sprocs/dbo.VineyardEstate_GetAll.sql
@@ -3,9 +3,31 @@
 GO
 
 ALTER PROCEDURE [dbo].[VineyardEstate_GetAll]
-
+    @Page INT = 1,
+    @PageSize INT = 10
 AS
 BEGIN
+    DECLARE @Offset INT;
+
+    IF(@Page = 1)
+    BEGIN 
+        SELECT @Offset = 0;
+    END
+    ELSE
+    BEGIN
+        SELECT @Offset = (@PageSize * (@Page - 1));
+    END
+
+    DECLARE @TotalPages INT, @TotalRecords INT;
+
+    SELECT @TotalRecords = COUNT(VE.[ID])
+    FROM [dbo].[VineyardEstate] AS VE;
+
+    SELECT @TotalPages = CEILING(CAST(COUNT(VE.[ID]) AS FLOAT)/@PageSize) 
+    FROM [dbo].[VineyardEstate] AS VE;
+
+    /*Paging info*/
+    SELECT @Page [Page], @PageSize [PageSize], @TotalPages [TotalPages], @TotalRecords [TotalRecords];
 
     SELECT
         VE.[ID],
@@ -16,6 +38,7 @@ BEGIN
         VE.[DateCreated],
         VE.[DateUpdated]
     FROM [dbo].[VineyardEstate] AS VE
-    ORDER BY VE.[Name] ASC;
+    ORDER BY VE.[Name] ASC
+    OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;
 
 END
