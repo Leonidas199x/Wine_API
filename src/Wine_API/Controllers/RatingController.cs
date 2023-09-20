7356bf44f943801e935a35a14c65c9fdae5c2d93
Leonidas199x <toby.w.prince@gmail.com>
2023-09-20 20:20:14 +0100

    Addition of Rating controller and get by wine ID endpoint

---
+++ src/Wine_API/Controllers/RatingController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Domain.Rating;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WineAPI.Controllers
+{
+    [Route("[controller]")]
+    public class RatingController : Controller
+    {
+        private readonly IRatingService _ratingService;
+        private readonly IMapper _mapper;
+
+        public RatingController(IRatingService ratingService, IMapper mapper)
+        {
+            _ratingService = ratingService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("WineId/{wineId}")]
+        public async Task<IActionResult> GetByWineId(int wineId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var ratings = await _ratingService.GetByWineId(wineId).ConfigureAwait(false);
+
+            return Ok(_mapper.Map<IEnumerable<DataContract.WineRating>>(ratings));
+        }
+    }
+}
