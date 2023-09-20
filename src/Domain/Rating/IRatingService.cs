1463906d115abfd9f69ff52efd322f315d636a8d
Leonidas199x <toby.w.prince@gmail.com>
2023-09-20 20:09:59 +0100

    Addition of rating service, and GetByWineId method

---
+++ src/Domain/Rating/IRatingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Rating
+{
+    public interface IRatingService
+    {
+        Task<IEnumerable<WineRating>> GetByWineId(int wineId);
+    }
+}
---
+++ src/Domain/Rating/RatingService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Rating
+{
+    public class RatingService : IRatingService
+    {
+        private readonly IRatingRepository _ratingRepository;
+
+        public RatingService(IRatingRepository ratingRepository) 
+        {
+            _ratingRepository = ratingRepository;
+        }
+
+        public async Task<IEnumerable<WineRating>> GetByWineId(int wineId)
+        {
+            return await _ratingRepository.GetByWineId(wineId).ConfigureAwait(false);
+        }
+    }
+}
