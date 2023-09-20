b1e6328f6aa2861593fc487407016f8a9c1b1c5f
Leonidas199x <toby.w.prince@gmail.com>
2023-09-20 20:25:48 +0100

    Addition of Rating Insert

---
+++ src/Database/Wine/sprocs/dbo.Rating_Insert.sql
@@ -0,0 +1,28 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Rating_Insert]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Rating_Insert] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Rating_Insert]
+    @DrinkerId INT,
+    @WineId INT,
+    @Rating INT,
+    @Note NVARCHAR(MAX)
+AS
+BEGIN
+
+    INSERT INTO [dbo].[Rating]
+    (
+        [DrinkerId],
+        [WineId],
+        [Rating],
+        [Note]
+    )
+    VALUES
+    (
+        @DrinkerId,
+        @WineId,
+        @Rating,
+        @Note
+    );
+
+END
