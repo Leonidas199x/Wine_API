63a3300fdb3a0dfadf2803d92d7e4d9ace50f88b
Leonidas199x <toby.w.prince@gmail.com>
2023-09-21 18:33:30 +0100

    Addition of get rating by ID

---
+++ src/Database/Wine/sprocs/dbo.Rating_GetById.sql
@@ -0,0 +1,25 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Rating_GetById]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Rating_GetById] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Rating_GetById]
+    @Id INT
+AS
+BEGIN
+
+    SELECT
+        R.[Id],
+        R.[WineId],
+        R.[Note],
+        R.[Rating]
+    FROM [dbo].[Rating] AS R
+    WHERE R.[Id] = @Id;
+
+    SELECT 
+        D.[Id],
+        D.[Name]
+    FROM [dbo].[Drinker] AS D
+    INNER JOIN [dbo].[Rating] AS R ON [R].[DrinkerId] = D.[Id]
+    WHERE R.[Id] = @Id;
+
+END
