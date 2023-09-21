1b3e1085cfcba6f89f5b9a0a894134ffbed2c53a
Leonidas199x <toby.w.prince@gmail.com>
2023-09-21 18:19:40 +0100

    Addition of rating delete

---
+++ src/Database/Wine/sprocs/dbo.Rating_Delete.sql
@@ -0,0 +1,14 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Rating_Delete]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Rating_Delete] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Rating_Delete]
+    @Id INT
+AS
+BEGIN
+
+    DELETE R
+    FROM [dbo].[Rating] AS R
+    WHERE R.[Id] = @Id;
+
+END
