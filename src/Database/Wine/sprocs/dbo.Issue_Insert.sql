27330e363160e862918ba991f00db8519a5289e9
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 18:14:21 +0100

    Addition of Issue Insert

---
+++ src/Database/Wine/sprocs/dbo.Issue_Insert.sql
@@ -0,0 +1,32 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Issue_Insert]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Issue_Insert] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Issue_Insert]
+    @WineId INT,
+    @Note NVARCHAR(MAX),
+    @Date DATE,
+    @Quantity INT
+AS
+BEGIN
+
+    INSERT INTO [dbo].[Issue]
+    (
+        [WineId],
+        [Note],
+        [Date],
+        [Quantity],
+        [DateUpdated],
+        [DateCreated]
+    )
+    VALUES
+    (
+        @WineId,
+        @Note,
+        @Date,
+        @Quantity,
+        GETDATE(),
+        GETDATE()
+    );
+
+END
