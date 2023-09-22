6916fc8e7b2438bc3acfa494058e8afe4dbba3b3
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 18:05:48 +0100

    Addition of Issue Delete sproc

---
+++ src/Database/Wine/sprocs/dbo.Issue_Delete.sql
@@ -0,0 +1,14 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Issue_Delete]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Issue_Delete] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Issue_Delete]
+    @Id INT
+AS
+BEGIN
+
+    DELETE I
+    FROM [dbo].[Issue] AS I
+    WHERE I.[Id] = @Id;
+
+END
