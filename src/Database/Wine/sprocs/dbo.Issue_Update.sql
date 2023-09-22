c322f7d01522fc7a54a644f2e05dd3aecccb6852
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 18:16:31 +0100

    Addition of issue update sproc

---
+++ src/Database/Wine/sprocs/dbo.Issue_Update.sql
@@ -0,0 +1,23 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Issue_Update]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Issue_Update] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Issue_Update]
+    @Id INT,
+    @WineId INT,
+    @Note NVARCHAR(MAX),
+    @Date DATE,
+    @Quantity INT
+AS
+BEGIN
+
+    UPDATE I
+    SET
+        I.[WineId] = @WineId,
+        I.[Note] = @Note,
+        I.[Date] = @Date,
+        I.[Quantity] = @Quantity
+    FROM [dbo].[Issue] AS I
+    WHERE I.[Id] = @Id;
+
+END
