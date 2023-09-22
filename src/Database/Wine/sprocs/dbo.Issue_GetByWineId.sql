f85329b64b00ecd1812c5e8013c33f00e522647f
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 18:10:01 +0100

    Addition of Issue Get By Wine Id

---
+++ src/Database/Wine/sprocs/dbo.Issue_GetByWineId.sql
@@ -0,0 +1,21 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Issue_GetByWineId]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Issue_GetByWineId] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Issue_GetByWineId]
+    @WineId INT
+AS
+BEGIN
+
+    SELECT
+        I.[Id],
+        I.[WineId],
+        I.[Note],
+        I.[Date],
+        I.[Quantity],
+        I.[DateCreated],
+        I.[DateUpdated]
+    FROM [dbo].[Issue] AS I
+    WHERE I.[WineId] = @WineId;
+
+END
