ea307d8d6ea35e78c3bf41bb410438368f9fe943
Toby Prince <toby.w.prince@outlook.com>
2023-10-04 22:46:04 +0100

    Addition of GetByDescription sproc for wine

---
+++ src/Database/Wine/sprocs/dbo.Wine_GetByDescription.sql
@@ -0,0 +1,25 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Wine_GetByDescription]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Wine_GetByDescription] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Wine_GetByDescription]
+    @Description NVARCHAR(100)
+AS
+BEGIN
+
+    SELECT
+        W.[ProducerID],
+        W.[RegionID],
+        W.[Vintage],
+        W.[QualityControlID],
+        W.[VinyardEstateID],
+        W.[WineTypeID],
+        W.[Description],
+        W.[ABV],
+        W.[Importer],
+        W.[InventoryLevel],
+        W.[ExclusiveToRetailerID]
+    FROM [dbo].[Wine] AS W
+    WHERE W.[Description] = @Description;
+
+END
