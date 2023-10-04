4ebd0ad7e2db835d61b97edc34b1f6ab4fac7623
Toby Prince <toby.w.prince@outlook.com>
2023-10-04 22:18:43 +0100

    Addition of WineCreate models

---
+++ src/DataContractCore/WineCreate.cs
@@ -0,0 +1,27 @@
+﻿namespace DataContract
+{
+    public class WineCreate
+    {
+        public string Description { get; set; }
+
+        public int? ProducerId { get; set; }
+
+        public int RegionId { get; set; }
+
+        public int? Vintage { get; set; }
+
+        public int? QualityControlId { get; set; }
+
+        public int? VineyardEstateId { get; set; }
+
+        public int WineTypeId { get; set; }
+
+        public int Abv { get; set; }
+
+        public string Importer { get; set; }
+
+        public int? InventoryLevel { get; set; }
+
+        public int? ExclusiveToRetailerId { get; set; }
+    }
+}
