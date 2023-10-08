562a584be4221824c758d51d5c47a85a69d1fc07
Toby Prince <toby.w.prince@outlook.com>
2023-10-08 15:31:10 +0100

    Updated Grape table so colour ID is not null

--- src/Database/Wine/up/0003.1 - Grape.CreateTable.sql
+++ src/Database/Wine/up/0003.1 - Grape.CreateTable.sql
@@ -3,7 +3,7 @@
     [ID] [int] IDENTITY(1,1) NOT NULL
         PRIMARY KEY,
     [Name] NVARCHAR(25) NOT NULL,
-    [GrapeColourID] INT NULL
+    [GrapeColourID] INT NOT NULL
         FOREIGN KEY REFERENCES [GrapeColour]([ID]),
     [Note] NVARCHAR(MAX) NULL,
     [DateCreated] DATETIME2(3) NOT NULL,
