5d1bd7296f46a4492d35bbc7d555b7a8477c2918
Toby Prince <toby.w.prince@outlook.com>
2023-10-08 14:49:57 +0100

    Addition of grape lookup sproc

---
+++ src/Database/Wine/sprocs/dbo.Grape_Lookup.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Grape_Lookup]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Grape_Lookup] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Grape_Lookup]
+AS
+BEGIN
+
+    SELECT
+        G.[ID],
+        G.[Name]
+    FROM [dbo].[Grape] AS G
+    ORDER BY G.[Name] ASC;
+
+END
