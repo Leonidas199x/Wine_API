4d47982464c9f94ba666fef408496503e75573e7
Toby Prince <toby.w.prince@outlook.com>
2023-10-08 12:39:59 +0100

    Addition of Wine_GetGrapeById sproc

---
+++ src/Database/Wine/sprocs/dbo.Wine_GetGrapeByWineId.sql
@@ -0,0 +1,24 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID('[dbo].[Wine_GetGrapeByWineId]') AND TYPE IN ('P','PC'))
+    EXEC ('CREATE PROCEDURE [dbo].[Wine_GetGrapeByWineId] AS BEGIN RETURN; END')
+GO
+
+ALTER PROCEDURE [dbo].[Wine_GetGrapeByWineId]
+    @WineId INT
+AS
+BEGIN
+
+    SELECT 
+        WG.[ID],
+        WG.[WineId],
+        WG.[GrapeId],
+        G.[ID],
+        G.[Name],
+        G.[GrapeColourID],
+        G.[Note],
+        G.[DateCreated],
+        G.[DateUpdated]
+    FROM [dbo].[GrapeWineBridge] AS WG
+    INNER JOIN [dbo].[Grape] AS G ON WG.[GrapeId] = G.[ID]
+    WHERE WG.[WineId] = @WineId;
+
+END
